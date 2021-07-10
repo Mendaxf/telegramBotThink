ed485c19922a0668db4f0e3c4fa1414867574dc3
Mendaxf <mendaxf@fastmail.org>
2021-07-10 13:49:55 +0700

    init

---
+++ src/main/java/com/bot/telegrambotthink/TelegramBotThinkApplication.java
@@ -0,0 +1,13 @@
+package com.bot.telegrambotthink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotThinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotThinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/telegrambotthink/TelegramBotThinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.telegrambotthink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotThinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
