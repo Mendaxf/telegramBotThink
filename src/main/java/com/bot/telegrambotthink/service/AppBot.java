5a9883b4d4446906ce3eeaa9e904de68c0b8dcd0
Mendaxf <mendaxf@fastmail.org>
2021-07-11 18:37:36 +0700

    Add Tinkoff methods

--- src/main/java/com/bot/telegrambotthink/service/AppBot.java
+++ src/main/java/com/bot/telegrambotthink/service/AppBot.java
@@ -1,5 +1,6 @@
 package com.bot.telegrambotthink.service;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -7,12 +8,18 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.tinkoff.invest.openapi.OpenApi;
+import ru.tinkoff.invest.openapi.OpenApiConfig;
+import ru.tinkoff.invest.openapi.model.rest.SandboxRegisterRequest;
+import ru.tinkoff.invest.openapi.okhttp.OkHttpOpenApi;
 
 @Slf4j
 @Component
 public class AppBot extends TelegramLongPollingBot {
     @Autowired
     private BotConfig botConfig;
+    @Autowired
+    private TinkConfig tinkConfig;
 
     @Override
     public String getBotUsername() {
@@ -24,6 +31,7 @@ public class AppBot extends TelegramLongPollingBot {
         return botConfig.getToken();
     }
 
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
         update.getUpdateId();
@@ -40,12 +48,35 @@ public class AppBot extends TelegramLongPollingBot {
             msg = update.getChannelPost().getText();
         }
         if(msg.contains("/start")){
-            builder.text("Hello");
+            builder.text("Hello! Please send me Tinkoff token");
             try {
                 execute(builder.build());
             }catch (TelegramApiException e){
                 log.info(e.getMessage().toString());
             }
         }
+        msg = update.getMessage().getText();
+        log.info(msg);
+
+        try {
+            OpenApi api = new OkHttpOpenApi(tinkConfig.getTtoken(),true);
+            log.info("Подключаемся");
+            if(api.isSandboxMode()) api.getSandboxContext().performRegistration(new SandboxRegisterRequest()).join();
+
+            final var portfolioCurrencies = api.getPortfolioContext().getPortfolioCurrencies(null).join();
+            log.info(portfolioCurrencies.toString());
+            builder.text(portfolioCurrencies.toString());
+            execute(builder.build());
+
+        }catch (Exception ex){
+            log.info(ex.getMessage());
+        }
+
+
+//        tinkConfig.setTtoken(msg);
+//        AppTinkoff appTinkoff = new AppTinkoff(tinkConfig.getTtoken(),true);
+//
+//        log.info(appTinkoff.getPortfolioContext().toString());
+       // execute(builder.build());
     }
 }
---
+++ src/main/java/com/bot/telegrambotthink/service/TinkConfig.java
@@ -0,0 +1,10 @@
+package com.bot.telegrambotthink.service;
+
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+public class TinkConfig {
+    private String Ttoken;
+}
