457bdb05b6c1aad55904caae0a4408b84c99b97c
Mendaxf <mendaxf@fastmail.org>
2021-07-12 20:27:46 +0700

    update first message

--- src/main/java/com/bot/telegrambotthink/service/AppBot.java
+++ src/main/java/com/bot/telegrambotthink/service/AppBot.java
@@ -8,10 +8,8 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.tinkoff.invest.openapi.OpenApi;
-import ru.tinkoff.invest.openapi.OpenApiConfig;
-import ru.tinkoff.invest.openapi.model.rest.SandboxRegisterRequest;
-import ru.tinkoff.invest.openapi.okhttp.OkHttpOpenApi;
+
+
 
 @Slf4j
 @Component
@@ -34,49 +32,22 @@ public class AppBot extends TelegramLongPollingBot {
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-        update.getUpdateId();
-        SendMessage.SendMessageBuilder builder = SendMessage.builder();
-        String msg;
-        String chatId;
-        if(update.getMessage() != null){
-            chatId = update.getMessage().getChatId().toString();
-            builder.chatId(chatId);
-            msg = update.getMessage().getText();
-        }else {
-            chatId = update.getChannelPost().getChatId().toString();
-            builder.chatId(chatId);
-            msg = update.getChannelPost().getText();
-        }
-        if(msg.contains("/start")){
-            builder.text("Hello! Please send me Tinkoff token");
-            try {
-                execute(builder.build());
-            }catch (TelegramApiException e){
-                log.info(e.getMessage().toString());
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message_text = update.getMessage().getText();
+            if(message_text.equals("/start")){
+                SendMessage message = new SendMessage();
+                message.setChatId(update.getMessage().getChatId().toString());
+                message.setText(update.getMessage().getText());
+                String firstName = update.getMessage().getFrom().getFirstName();
+                message.setText("Hello! "+firstName +" Please send me Tinkoff token");
+                try {
+                    execute(message);
+                    message.setText("");
+                }catch (TelegramApiException e){
+                    log.info(e.getMessage());
+                }
             }
         }
-        msg = update.getMessage().getText();
-        log.info(msg);
 
-        try {
-            OpenApi api = new OkHttpOpenApi(tinkConfig.getTtoken(),true);
-            log.info("Подключаемся");
-            if(api.isSandboxMode()) api.getSandboxContext().performRegistration(new SandboxRegisterRequest()).join();
-
-            final var portfolioCurrencies = api.getPortfolioContext().getPortfolioCurrencies(null).join();
-            log.info(portfolioCurrencies.toString());
-            builder.text(portfolioCurrencies.toString());
-            execute(builder.build());
-
-        }catch (Exception ex){
-            log.info(ex.getMessage());
-        }
-
-
-//        tinkConfig.setTtoken(msg);
-//        AppTinkoff appTinkoff = new AppTinkoff(tinkConfig.getTtoken(),true);
-//
-//        log.info(appTinkoff.getPortfolioContext().toString());
-       // execute(builder.build());
     }
 }
